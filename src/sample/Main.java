c6885b98672e52b1f2cfe7ba05997d2a63b6c7e1
LaviniaRus <lavinia.rus20@gmail.com>
2021-03-21 17:20:11 +0200

    new

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    //change
 
     @Override
     public void start(Stage primaryStage) throws Exception{
